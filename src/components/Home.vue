a92e065606f6e54b0e37372d9c6733b097f62c05
takumiiimo <[assistant@gmail.com]>
2021-05-13 15:40:11 +0900

    section 10

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,28 @@
 <template>
-  <p>Home</p>
-</template>     
+  <p v-border:solid.round.shadow="{ width: '3px', color: 'brown' }">
+    {{ tmpData }}
+  </p>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tmpData: "Home",
+    };
+  },
+  directives: {
+    border(el, binding) {
+      el.style.border = "solid black 2px";
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)";
+      }
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,18 @@ import LikeNumber from './components/LikeNumber.vue'
 Vue.config.productionTip = false
 
 Vue.component('LikeNumber', LikeNumber);
+// Vue.directive("border", function (el, binding) {
+//   el.style.border = "solid black 2px";
+//   el.style.borderWidth = binding.value.width;
+//   el.style.borderColor = binding.value.color;
+//   el.style.borderStyle = binding.arg;
+//   if (binding.modifiers.round) {
+//     el.style.borderRadius = "0.5rem"
+//   }
+//   if (binding.modifiers.shadow) {
+//     el.style.boxShadow = "0 2px 5px rgba(0, 0, 0, 0.26)"
+//   }
+// });
 
 new Vue({
   render: h => h(App),
